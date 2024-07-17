f397496e49d099bca2c0e25e52eef6f010eb8bee
ingridbatista <ingridmarianb92@gmail.com>
2024-07-17 11:24:10 -0300

    Mapeamento Objeto Relacional(ORM)

---
+++ src/main/java/com/dio/ingrid/domain/model/Feature.java
@@ -0,0 +1,7 @@
+package com.dio.ingrid.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem{
+}
---
+++ src/main/java/com/dio/ingrid/domain/model/News.java
@@ -0,0 +1,7 @@
+package com.dio.ingrid.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem{
+}
